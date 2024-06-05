b22ee34c63b665a501689d54ede04d606002e0c5
EMNayon <emranmasudnayon.017@gmail.com>
2024-06-05 15:26:53 +0600

    booking pdf desing almost done

--- resources/views/booking-pdf.blade.php
+++ resources/views/booking-pdf.blade.php
@@ -10,14 +10,17 @@
         * {
             margin: 0;
             padding: 0;
-            font-size:10px;
+            /* font-size: 10px; */
+        }
+        body {
+            font-size: 10px;
         }
 
         .container {
-            width: 70%;
+            width: 90%;
             margin: 3% auto;
             padding: 10px;
-            border: 1px solid red;
+            /* border: 1px solid red; */
         }
 
         .left {
@@ -33,11 +36,11 @@
         }
 
         .navy-blue {
-            color: navy
+            color: rgb(9, 9, 243)
         }
 
         .sky-blue {
-            color: skyblue
+            color: rgb(16, 169, 230)
         }
 
         .w-68 {
@@ -59,82 +62,83 @@
         {{-- heading section --}}
         <div>
             <div class="heaading w-68 left">
-                <h1><span class="navy-blue">Booking</span><span class="sky-blue">.com</span></h1>
+                <h1 style="font-size: 32px;" ><span class="navy-blue">Booking</span><span class="sky-blue">.com</span></h1>
             </div>
             <div class="w-28 right">
                 <div>
-                    <p>Booking Confirmation</p>
-                    <p><span class="red">Confiramtion Number:</span> <span class="sky-blue">{{ $booking->confirmation_number }}</span>
+                    <h2>Booking Confirmation</h2>
+                    <p><span class="red">CONFIRMATION NUMBER:</span> <span
+                            class="sky-blue" style="font-weight: bold;">{{ $booking->confirmation_number }}</span>
                     </p>
-                    <p><span class="red">Pin Code: </span><span class="sky-blue">{{ $booking->pin_code }}</span></p>
+                    <p><span class="red">PIN CODE: </span><span class="sky-blue" style="font-weight: bold;">{{ $booking->pin_code }}</span></p>
                 </div>
             </div>
 
             <div class="clear"></div>
         </div>
 
-        <div style="background-color: #cbcbcb;padding:12px;">
-            <div style="">
-                <div class="left" style="width:120px; height:120px; background-color:green; margin-right:15px;">logo
+        <div style="background-color: #f8f4f4;padding:12px;border-top: 2px solid black; ">
+            <div style="border-bottom: 1px solid gray; ">
+                <div class="left" style="width:80px; height:80px; background-color:green; margin-right:5px; margin-bottom: 10px;">logo
                 </div>
-                <div class="left"
-                    style="margin-right:10px; margin-left:10px; border-right:1px solid grey; padding-right: 10px;">
-                    <p><span class="red"><b>{{ $booking->hotel->name }}</b> </span></p>
-                    <p><span class="red">Address:</span>{{ $booking->hotel->city->name}},{{   $booking->hotel->city->state->name}} , {{ $booking->hotel->city->state->country->name }}</p>
-                    <p><span>Phone</span> <span>{{ $booking->phone }}</span> </p>
-                    <p><span class="red">GPS Coordinates</span> <span>N {{ $booking->hotel->latitude }}, W {{ $booking->hotel->longitude }}</span></p>
+                <div class="left" style="margin-right:10px; margin-left:10px;  line-height: 1.6;">
+                    <h3><span class="red"><b>{{ $booking->hotel->name }}</b> </span></h3>
+                    <p><span class="red"><b>Address :</b></span>{{ $booking->hotel->city->name }},{{ $booking->hotel->city->state->name }} , {{ $booking->hotel->city->state->country->name }}</p>
+                    <p><span class="red"><b>Phone :</b> </span> <span>{{ $booking->phone }}</span> </p>
+                    <p><span class="red"><b>GPS Coordinates : </b></span> <span>N {{ $booking->hotel->latitude }} , W {{ $booking->hotel->longitude }}</span></p>
                 </div>
-                <div class="left" style="border-right: 1px solid grey; margin-right:15px;padding-right:15px;">
+                <div class="left" style="border-right: 1px solid grey; margin-right:15px;padding-right:15px; border-left:1px solid grey; padding-left: 10px; text-align:center;">
                     <div>
-                        <p class="red">CHECK IN</p>
-                        <h1>{{ $check_in_date }}</h1>
-                        <p class="red">{{ $check_in_month }}</p>
+                        <p class="red" style="font-size: 8px;">CHECK IN</p>
+                        <h1 style="font-size: 24px;">{{ $check_in_date }}</h1>
+                        <h3 class="red" style="font-weight: bold;">{{ $check_in_month }}</h3>
                         <p class="red"><i>{{ $check_in_day }}</i></p>
                         <p class="red"> from 15.00</p>
                     </div>
                 </div>
-                <div class="left" style="border-right: 1px solid grey; margin-right:15px;padding-right:15px;">
+                <div class="left" style="border-right: 1px solid grey; margin-right:15px;padding-right:15px; text-align:center;">
                     <div>
-                        <p class="red">CHECK OUT</p>
-                        <h1>{{ $check_out_date }}</h1>
-                        <p class="red">{{ $check_out_month }}</p>
+                        <p class="red" style="font-size: 8px;">CHECK OUT</p>
+                        <h1 style="font-size: 24px;">{{ $check_out_date }}</h1>
+                        <h3 class="red" style="font-weight: bold;">{{ $check_out_month }}</h3>
                         <p class="red"><i>{{ $check_out_day }}</i></p>
                         <p class="red"> until 12.00</p>
                     </div>
                 </div>
 
-                <div class="left" style="border-right: 1px solid grey; margin-right:15px;padding-right:15px;">
-                    <div>
+                <div style="">
+                    <div class="left" style="margin-right: 15px;">
                         <p class="red">ROOMS</p>
-                        <h1>{{ $booking->rooms }}/</h1>
+                        <h1 style="font-size: 24px;">{{ $booking->rooms }}/</h1>
                     </div>
-                </div>
-                <div class="left" style=" margin-right:15px;padding-right:15px;">
-                    <div>
-                        <p> <span class="red">NIGTHT</span><span>S</span></p>
-                        <p>{{ $booking->nights }}</p>
+                    <div class="left">
+                        <p> <span class="red">NIGHT</span><span>S</span></p>
+                        <h1 style="font-size: 24px;">{{ $booking->nights }}</h1>
                     </div>
                 </div>
                 <div class="clear"></div>
+                {{-- <div class="left" style=" margin-right:15px;padding-right:15px;">
+
+                </div> --}}
             </div>
 
-            <div>
-                <h1 class="red">PRICE</h1>
+            <div style="margin-top: 5px;">
+                <h2 class="red">PRICE</h2>
                 <div>
-                    <div class="left">{{ $booking->rooms }} room/s</div>
-                    <div class="right">BDT {{ $booking->price }}</div>
+                    <div class="left">{{ $booking->rooms }} room</div>
+                    <div class="right" style="color: gray;">BDT {{ $booking->price }}</div>
                     <div class="clear"></div>
                 </div>
 
                 <div>
-                    <h1 class="red left">Price</h1>
-                    <h1 class="right"><span class="red">approx</span>{{ $booking->price }}</h1>
+                    <h2 class="red left">Price</h2>
+                    <h1 class="right" style="color: gray"><span class="red" style="font-size: 16px;">approx </span>BDT {{ $booking->price }}</h1>
                     <div class="clear"></div>
                 </div>
 
                 <div>
                     <p class="left">(for 1 guest)</p>
-                    <p class=right>CAD 90.343</p>
+                    <p class="right" style="color: gray;">CAD 90.343</p>
                     <p class="clear"></p>
                 </div>
                 <div>
@@ -145,68 +149,71 @@
                 </div>
                 <div>
                     <p class="red left">TaX ({{ $booking->tax }})</p>
-                    <p class="right">approx BDT{{ $booking->total_price }}</p>
+                    <p class="right" style="color: gray;"><span style="color: black;">approx.</span> BDT {{ $booking->total_price }}</p>
                     <p class="clear"></p>
                 </div>
                 <div>
                     <p class="left">(taxes included)</p>
-                    <h1 class="right">You will pay 834783.3478 in CAD</h1>
-                    <p>right</p>
+                    <p class="right" style="font-size: 14px; color:gray;">You'll pay 834783.3478 in CAD</p>
+                    <p class="clear"></p>
                 </div>
 
-                <div>
-                    <h3>The final price shown is the amount you'll pay to the property.</h3>
+                <div class="">
+                    <h2>The final price shown is the amount you'll pay to the property.</h2>
                 </div>
 
                 <div style="border-bottom:1px solid grey;">
-                    <p>Booking.com doesn't charge guests any reservation, administration, or other fees.
+                    <p style=" width: 50%; margin-bottom:5px;">Booking.com doesn't charge guests any reservation, administration, or other fees.
                         Your card issuer may charge you a foreign transaction fee.</p>
                 </div>
 
                 <div>
-                    <h3>Payment Info</h3>
+                    <h2>Payment Info</h2>
                 </div>
                 <div style="border-bottom:1px solid grey;">
-                    <p>
-                        The Omni King Edward Hotel handles all payments.
+                    <p style="margin-bottom: 5px;">
+                        The Omni King Edward Hotel handles all payments. <br>
                         This property accepts the following forms of payment: American Express, Visa, Mastercard, Diners
                         Club, JCB, Discover, UnionPay credit card
                     </p>
                 </div>
 
                 <div>
-                    <p>Currency & Exchange Rate Ino</p>
-                </div>
-
-                <div>
-                    <p>You'll pay The Omni King Edward Hotel in CAD according to the exchange rate on the day of
-                        payment.
-                        The amount displayed in BDT is just an estimate based on today's exchange rate for CAD.</p>
-                </div>
-
-                <div>
-                    <h1>Additional Info</h1>
+                    <p style="font-size: 14px;">Currency & Exchange Rate Ino</p>
                 </div>
 
                 <div style="border-bottom:1px solid grey;">
-                    <p>Note that additional supplements (e.g. an extra bed) aren't added in this total.
-                        If you cancel, applicable taxes may still be charged by the property.
+                    <p style="margin-bottom: 5px;">You'll pay The Omni King Edward Hotel in CAD according to the exchange rate on the day of
+                        payment. <br>
+                        The amount displayed in BDT is just an estimate based on <b>today's</b> exchange rate for CAD.</p>
+                </div>
+
+                <div>
+                    <h2>Additional Info</h2>
+                </div>
+
+                <div style="border-bottom:1px solid grey;">
+                    <p style="margin-bottom: 5px;">
+                        Note that additional supplements (e.g. an extra bed) aren't added in this total. <br>
+                        If you cancel, applicable taxes may still be charged by the property. <br>
                         If you don’t show up for this booking, and you don’t cancel beforehand, the property is liable
-                        to charge you the full reservation amount.
-                        Remember to read the Important info below – it could contain important details not mentioned
+                        to charge you the full reservation amount. <br>
+                        Remember to read the <b>Important info</b> below – it could contain important details not mentioned
                         here.</p>
                 </div>
 
                 <div style="height:300px;width:100%;border-top:1px solid  black; border-bottom:1px solid black;"></div>
 
-                <div>
-                    <div class="left">
-                        <p class="red">Deluxe Room - {{ $booking->deluxe_room }}</p>
-                        <p><span class="red">Guest name:</span> {{ $booking->guest_name }} / for 3 aduts, 2 childred, up to 0 year
+                <div style="border-bottom: 1px solid black; border-right: 1px solid black; border-bottom: 1px solid black; margin-bottom: 5px;">
+                    <div class="left" style="width: 65%; ">
+                        <div style="border-right:1px solid black; margin-top:5px;">
+                            <h2 class="red">Deluxe Room - {{ $booking->deluxe_room }}</h2>
+                        <p><span class="red"><b>Guest name : </b></span> {{ $booking->guest_name }} / for 3 aduts, 2
+                            childred, up to 0 year
                         </p>
-                        <p><span class="red">No Meal is Included in this room rate</span></p>
+                        <p style="margin-bottom: 5px;"><span class="red"> <b>Meal Plan :</b> </span>No Meal is Included in this room rate</p>
 
-                        <p>Private Bathroom • View • Free toiletries • Bathrobe • Air conditioning • Safe • Streaming
+                        <p style="padding-right: 15px; margin-bottom: 4px; ">Private Bathroom • View • Free toiletries • Bathrobe • Air conditioning • Safe • Streaming
                             service (like Netflix) •
                             Toilet • Bathtub or shower • Towels • Linens • Desk • Slippers • Refrigerator • Telephone •
                             Tea/Coffee maker •
@@ -216,33 +223,35 @@
                             • Upper floors
                             accessible by elevator • Toilet paper</p>
 
-                        <p><span class="red">Bed Size(s):</span> 1 king bed (181-210cm width)</p>
+                        <p style="margin-bottom: 5px;"><span class="red">Bed Size(s):</span> 1 king bed (181-210cm width)</p>
+                        </div>
                     </div>
 
 
-                    <div class="right">
-                        <p><b>payment</b> no payment is needed</p>
+                    <div class="right" style="margin-top: 20px; width: 34%;">
+                        <p><b>Prepayment</b> no prepayment is needed</p>
 
-                        <p class="red">Cancelation cost</p>
+                        <p class="red"><b>Cancelation cost :</b></p>
 
-                        <p>until December 12, 2023 11:59 PM [EST]: CAD 0</p>
-                        <p>from December 13, 2023 12:00 AM [EST]:</p>
-                        <p class="red">CAD 324.78 –
+                        <p style="font-size: 9px;">until December 12, 2023 11:59 PM [EST]: CAD 0</p>
+                        <p style="font-size: 9px;">from December 13, 2023 12:00 AM [EST]: <span class="red">CAD 324.78 –</span></p>
+                        <p class="red">
                             Changing the dates of your stay isn't possible.</p>
                     </div>
                     <div class="clear"></div>
                 </div>
 
 
-                <div>
-                    <div class="left">
-                        <h1>Important Notice</h1>
+                <div style="border-bottom: 1px solid black;">
+                    <div class="left" style="width: 48%; padding-right: 15px;">
+                        <h3>Important Information</h3>
                         <p>
                             Credit card is required at check in for incidentals at 150 CAD. For Debit
                             Card paying customers, full room and tax plus 250 CAD for standard
                             rooms and 500 CAD for suites will be obtained for security deposit at
                             check in.
                         </p>
+                        <br>
                         <p>
                             Guests are required to show a photo ID and credit card upon check-in.
                             Please note that all Special Requests are subject to availability and
@@ -250,10 +259,10 @@
                         </p>
                     </div>
 
-                    <div class="right">
-                        <h1>Hotel Policies</h1>
-                        <p>Guest parking</p>
-                        <ul>
+                    <div class="right" style="width: 48%; margin-left: 15px;">
+                        <h3>Hotel Policies</h3>
+                        <p ><b>Guest parking</b></p>
+                        <ul style="margin-left: 10px;">
                             <li> Public parking is possible on site (reservation is not needed) and
                                 costs CAD 60 per day.</li>
 
@@ -261,7 +270,7 @@
                                 WiFi is available in the rooms and costs CAD 14.95 per 24 hours.
                             </li>
                         </ul>
-                       
+
 
                     </div>
                     <div class="clear"></div>
@@ -270,8 +279,8 @@
 
                 <div>
                     <div>
-                        <p>You can always view, change or cancel your booking online at:</p>
-                        <p>your.booking.com</p>
+                        <p><b>You can always view, change or cancel your booking online at:</b></p>
+                        <p style="text-decoration: underline">your.booking.com</p>
                         <p>For any questions related to the property, you can contact The Omni
                             King Edward Hotel directly at: +1 416 863 9700</p>
                         <p>Or contact us by phone - we're available 24 hours a day:</p>
@@ -281,7 +290,7 @@
                         <p class="sky-blue" style="text-decoration: underline">See safety resource center</p>
                         <p>We’ve gathered the most important local phone numbers to help give
                             you complete peace of mind during your stay in Canada.</p>
-                            <p class="sky-blue" style="text-decoration: underline">See local emergency services</p>
+                        <p class="sky-blue" style="text-decoration: underline">See local emergency services</p>
                     </div>
                 </div>
 
